ee803ca19d3326d44d4a189b867d57f0c8f1d3c2
Higor Oliveira <56976806+walker05374@users.noreply.github.com>
2023-07-11 14:55:13 -0300

    up

--- jornada_maternal/app/urls.py
+++ jornada_maternal/app/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('test', views.test, name='test'),
+    path('login', views.login, name='login'),
+    path('cadastro', views.cadastro, name='cadastro'),
 ]
--- jornada_maternal/app/views.py
+++ jornada_maternal/app/views.py
@@ -2,5 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, '1.inicio.html')
+def login(request):
+    return render(request, 'login.html')
+
+def redefenir(request):
+    return render(request, 'redefenir.html')
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def abaVacina(request):
+    return render(request, 'abaVacina.html')
+
+def abaPreNatal(request):
+    return render(request, 'abaPreNatal.html')
+
+def abaMais(request):
+    return render(request, 'abaMais.html')
+
+def subAmamentacao(request):
+    return render(request, 'abaMais.html')
+
+def subNoticias(request):
+    return render(request, 'subNoticias.html')
+
+def subChat(request):
+    return render(request, 'subChat.html')
+
+
