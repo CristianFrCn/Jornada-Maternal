12c1ddaaa13ef955e823043c32e3f9ff61edf3b4
Higor Oliveira <56976806+walker05374@users.noreply.github.com>
2023-07-12 16:25:36 -0300

    up

--- jornada_maternal/jornada_maternal/settings.py
+++ jornada_maternal/jornada_maternal/settings.py
@@ -32,7 +32,7 @@ ROOT_URLCONF = 'jornada_maternal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -90,8 +90,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    'static',
+    'media',
 ]
 
 MEDIA_URL = '/media/'
