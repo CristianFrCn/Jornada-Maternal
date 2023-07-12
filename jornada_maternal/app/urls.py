5475ef62aab4c71dd81d5f17005ea9828c4309e8
Higor Oliveira <56976806+walker05374@users.noreply.github.com>
2023-07-12 18:59:35 -0300

    organizacao 0

--- jornada_maternal/app/urls.py
+++ jornada_maternal/app/urls.py
@@ -4,5 +4,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('redefenir/', views.redefenir, name='redefenir' ),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('inicio/', views.inicio, name='inicio'),
+    path('abaVacina/', views.abaVacina, name='abaVacina'),
+    path('abaPreNatal/', views.abaPreNatal, name='abaVacina'),
+    path('abaMais/', views.abaMais, name='abaMais'),
+    path('subAmamentacao/', views.subAmamentacao, name='subAmamentacao'),
+    path('subNoticias/', views.subNoticias, name='subNoticias'),
+
     ]
--- jornada_maternal/app/views.py
+++ jornada_maternal/app/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def redefenir(request):
+    return render(request, 'redefenir.html')
+def cadastro(request):
+    return render(request, 'cadastro.html')
+def inicio(request):
+    return render(request, 'inicio.html')
+def abaVacina(request):
+    return render(request, 'abaVacina.html')
+def abaPreNatal(request):
+    return render(request, 'abaPreNatal.html')
+def abaMais(request):
+    return render(request, 'abaMais.html')
+def subAmamentacao(request):
+    return render(request, 'subAmamentacao.html')
+def subNoticias(request):
+    return render(request, 'subNoticias.html')
+def subChat(request):
+    return render(request, 'redefenir.html')
