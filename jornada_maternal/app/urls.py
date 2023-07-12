3d4bb38b3f3dc5994371c95f38a507f7cfe89dbc
Higor Oliveira <56976806+walker05374@users.noreply.github.com>
2023-07-12 11:40:00 -0300

    correcao .py

--- jornada_maternal/app/urls.py
+++ jornada_maternal/app/urls.py
@@ -4,8 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('test', views.test, name='test'),
-    path('login', views.login, name='login'),
-    path('cadastro', views.cadastro, name='cadastro'),
+    path('login', views.home, name='index.html'),
+
 ]
--- jornada_maternal/jornada_maternal/urls.py
+++ jornada_maternal/jornada_maternal/urls.py
@@ -1,10 +1,8 @@
-
 from django.contrib import admin
 
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jornada-maternal/', include('app.urls'))
+    path('jornada_maternal/', include('app.urls')),
 ]
-
